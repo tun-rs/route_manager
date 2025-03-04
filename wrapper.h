30f4b176995557ba0abbb87ba79b033975eefb80
lbl <1791778603@qq.com>
2025-03-04 22:49:31 +0800

    macos/freebsd wrapper

---
+++ wrapper.h
@@ -0,0 +1,8 @@
+#include <sys/param.h>
+#include <sys/sysctl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <net/route.h>
+#include <net/if_dl.h>
+#include <net/if.h>
