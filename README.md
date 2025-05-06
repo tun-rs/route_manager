cfe72c19023cf2b284819fe1c5a7bbcafa37e73b
lbl <1791778603@qq.com>
2025-05-06 20:32:31 +0800

    update doc

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ with support for asynchronous or synchronous monitoring of route changes.
 2. Supports choosing between Tokio and async-io for asynchronous I/O operations.
 
 ## Example:
-
+Asynchronous API
 ```rust
 use route_manager::{AsyncRouteManager, Route};
 use std::time::Duration;
@@ -44,7 +44,37 @@ pub async fn main() {
     tokio::time::sleep(Duration::from_secs(1)).await;
 }
 ```
+Synchronous API
+```rust
+use route_manager::{Route, RouteManager};
+use std::thread;
+use std::time::Duration;
 
+pub fn main() {
+    let mut route_listener = RouteManager::listener().unwrap();
+    #[cfg(feature = "shutdown")]
+    let shutdown_handle = route_listener.shutdown_handle().unwrap();
+    thread::spawn(move || {
+        while let Ok(route) = route_listener.listen() {
+            println!("listen {route}");
+        }
+        println!("========= end =========");
+    });
+    // Need to set up the correct gateway
+    let route = Route::new("192.168.2.0".parse().unwrap(), 24).with_if_index(1);
+    let mut manager = RouteManager::new().unwrap();
+
+    let result = manager.add(&route);
+    println!("route add {route} {result:?}");
+    thread::sleep(Duration::from_secs(1));
+    let result = manager.delete(&route);
+    println!("route delete {route} {result:?}");
+    thread::sleep(Duration::from_secs(1));
+    #[cfg(feature = "shutdown")]
+    shutdown_handle.shutdown().unwrap();
+    thread::sleep(Duration::from_secs(100));
+}
+```
 ## Reference project
 
 - [net-route](https://github.com/johnyburd/net-route)
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,58 @@
+/*!
+## Example:
+### Asynchronous API
+```rust,no_run
+use route_manager::{AsyncRouteManager, Route};
+use std::time::Duration;
+#[cfg(feature = "async")]
+#[tokio::main]
+pub async fn main() {
+    let mut route_listener = AsyncRouteManager::listener().unwrap();
+    tokio::spawn(async move {
+        while let Ok(route) = route_listener.listen().await {
+            println!("listen {route}");
+        }
+    });
+    // Need to set up the correct gateway
+    let route = Route::new("192.168.2.0".parse().unwrap(), 24).with_if_index(1);
+    let mut manager = AsyncRouteManager::new().unwrap();
+    let result = manager.add(&route).await;
+    println!("route add {route} {result:?}");
+    tokio::time::sleep(Duration::from_secs(1)).await;
+    let result = manager.delete(&route).await;
+    println!("route delete {route} {result:?}");
+    tokio::time::sleep(Duration::from_secs(1)).await;
+}
+```
+### Synchronous API
+```rust,no_run
+use route_manager::{Route, RouteManager};
+use std::thread;
+use std::time::Duration;
+let mut route_listener = RouteManager::listener().unwrap();
+#[cfg(feature = "shutdown")]
+let shutdown_handle = route_listener.shutdown_handle().unwrap();
+thread::spawn(move || {
+    while let Ok(route) = route_listener.listen() {
+        println!("listen {route}");
+    }
+    println!("========= end =========");
+});
+// Need to set up the correct gateway
+let route = Route::new("192.168.2.0".parse().unwrap(), 24).with_if_index(1);
+let mut manager = RouteManager::new().unwrap();
+
+let result = manager.add(&route);
+println!("route add {route} {result:?}");
+thread::sleep(Duration::from_secs(1));
+let result = manager.delete(&route);
+println!("route delete {route} {result:?}");
+thread::sleep(Duration::from_secs(1));
+#[cfg(feature = "shutdown")]
+shutdown_handle.shutdown().unwrap();
+thread::sleep(Duration::from_secs(100));
+```
+ */
 mod common;
 #[cfg(windows)]
 mod windows;
