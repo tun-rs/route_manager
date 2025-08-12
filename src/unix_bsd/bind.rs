9055e9aa553d4915af63a27af0b3c45e8f581d59
lbl <1791778603@qq.com>
2025-08-12 15:01:46 +0800

    rename feature

--- src/unix_bsd/bind.rs
+++ src/unix_bsd/bind.rs
@@ -1,16 +1,16 @@
 #![allow(warnings)]
 #[cfg(not(docsrs))]
-#[cfg(feature = "build-bindings")]
+#[cfg(feature = "bindgen")]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-#[cfg(all(target_os = "freebsd", any(docsrs, not(feature = "build-bindings"))))]
+#[cfg(all(target_os = "freebsd", any(docsrs, not(feature = "bindgen"))))]
 include!("freebsd_bindings.rs");
 
-#[cfg(all(target_os = "openbsd", any(docsrs, not(feature = "build-bindings"))))]
+#[cfg(all(target_os = "openbsd", any(docsrs, not(feature = "bindgen"))))]
 include!("openbsd_bindings.rs");
 
-#[cfg(all(target_os = "macos", any(docsrs, not(feature = "build-bindings"))))]
+#[cfg(all(target_os = "macos", any(docsrs, not(feature = "bindgen"))))]
 include!("macos_bindings.rs");
 
-#[cfg(all(target_os = "netbsd", any(docsrs, not(feature = "build-bindings"))))]
+#[cfg(all(target_os = "netbsd", any(docsrs, not(feature = "bindgen"))))]
 include!("netbsd_bindings.rs");
